798549130afc46b7e3e0183607394de4442b744c
qtalam <qtalam@gmail.com>
2017-12-19 00:08:14 +0600

    Add URL

--- plugin.rb
+++ plugin.rb
@@ -2,5 +2,6 @@
 # about: A Discourse plugin that makes the search bar visible in the header
 # version: 0.1
 # authors: Angus McLeod
+# url: https://github.com/qtalam/discourse-header-search
 
 register_asset 'stylesheets/header-search.scss', :desktop
